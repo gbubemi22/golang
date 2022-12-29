bd33a4f1e0466abceb53815681c098c89f926acd
gbubemi22 <slimnfine22@gmail.com>
2022-12-29 16:17:00 +0100

    structs in go

--- structs.go
+++ structs.go
@@ -1,18 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+
+func getInput( prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	imput, err := r.ReadString('\n')
+	return strings.TrimSpace(imput), err
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	// fmt.Println("create a new bill name: ")
+	// name, _ := reader.ReadString('\n')
+	// name = strings.TrimSpace(name)
+     name, _ := getInput("create a new bill name: ", reader)
+
+	b := newBill(name) 
+	fmt.Println("created the bill - ", b.name)
+	return b 
+} 
 
 func main() {
 
-	mybill := newBill("gbubemi's bill")
+	mybill := createBill()
+	// mybill.addItem("onion soup", 4.50)
+	// mybill.addItem("veg pie", 8.95)
+	// mybill.addItem("toffee pudding", 4.95)
+	// mybill.addItem("coffee", 3.25)
 
+	fmt.Println(mybill)
 
-     mybill.addItem("onion soup", 4.50)
-     mybill.addItem("veg pie", 8.95)
-     mybill.addItem("toffee pudding", 4.95)
-     mybill.addItem("coffee", 3.25)
-	
-	mybill.updateTip(10)
-
-	fmt.Println(mybill.format())
 }
